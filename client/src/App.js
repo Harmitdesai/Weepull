345109af0923af8e2b0bcaaf31f483392d347041
Harmit2789 <Harmit9357@gmail.com>
2024-08-08 17:23:03 -0700

    Update App.js
    ..

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import { Route, Routes } from 'react-router-dom'
 import Services from "./Services";
 import Login from "./Login";
-// import Signup from "./Signup";
+import Signup from "./Signup";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/services" element = { <Services /> }/>
         <Route path="/Login" element = { <Login />} />
+        <Route path='/Signup' element = {<Signup/>}/>
       </Routes>
     </div>
   );
